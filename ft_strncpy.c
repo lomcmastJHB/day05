e309f166bf387ac462b3a4747d0d05affde75293
lomcmastJHB <67949114+lomcmastJHB@users.noreply.github.com>
2020-07-16 19:05:40 +0100

    Create ft_strncpy.c

---
+++ ft_strncpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+char  *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+  int   i;
+  
+  i = 0;
+  while (src[i] != '\0')
+  {
+    dest[i] = src[i];
+    i ++;
+  }
+  
